860206336db758940758c74239a7aceb28256c30
honorableisrael <honorableisrael>
2021-11-10 07:13:40 +0100

    added deployment kit

---
+++ stepformdeployment/precache-manifest.4ba444b9f44a946f23333439d0b5ee06.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "082a88835288604d61c4f8c1e30ecafe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cdb86fe0112a64af3fa2",
+    "url": "/static/css/main.63065050.chunk.css"
+  },
+  {
+    "revision": "62801cf89da31b6985ed",
+    "url": "/static/js/2.d47b559b.chunk.js"
+  },
+  {
+    "revision": "748d5fd8a7e7493724665d4098123404",
+    "url": "/static/js/2.d47b559b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cdb86fe0112a64af3fa2",
+    "url": "/static/js/main.5513fce7.chunk.js"
+  },
+  {
+    "revision": "177d52efa2c7db582a05",
+    "url": "/static/js/runtime-main.5b50bb1e.js"
+  },
+  {
+    "revision": "f41e4eb15ade3b906010a76c63ddadff",
+    "url": "/static/media/BRFirma-Regular.f41e4eb1.ttf"
+  },
+  {
+    "revision": "0868698250703d96f8d99159010e7d2a",
+    "url": "/static/media/coffee_3.08686982.jpeg"
+  }
+]);
